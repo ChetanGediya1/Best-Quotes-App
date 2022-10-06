90e05336284e08bd7d2f7137bf1e1270848c767c
Chetan <chetangediya5>
2022-10-06 19:15:58 +0530

    Best Quotes final

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText edWeg,edHei;
+        TextView txtRes,txtInter;
+        Button btnRes,btnReset;
+
+        edWeg=(EditText) findViewById(R.id.edWeg);
+        edHei=(EditText) findViewById(R.id.edHei);
+
+        txtRes=(TextView) findViewById(R.id.txtRes);
+        txtInter=(TextView) findViewById(R.id.txtInter);
+
+        btnRes=(Button) findViewById(R.id.btnRes);
+        btnReset=(Button) findViewById(R.id.btnReset);
+
+        btnRes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strweg = edWeg.getText().toString();
+                String strhei = edHei.getText().toString();
+
+                if (strweg.equals("")){
+                    edWeg.setError("Please Enter Your Weight");
+                    edWeg.requestFocus();
+                    return;
+                }
+
+                if (strhei.equals("")) {
+                    edHei.setError("Please Enter Your Height");
+                    edHei.requestFocus();
+                    return;
+                }
+                float weight = Float.parseFloat(strweg);
+                float height= Float.parseFloat(strhei)/100;
+
+                float bmiVlaue = BMICalculate(weight,height);
+
+                txtInter.setText(interpreteBMI(bmiVlaue));
+                txtRes.setText("BMI= "+bmiVlaue);
+
+            }
+        });
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                edHei.setText("");
+                edWeg.setText("");
+                txtInter.setText("");
+                txtRes.setText("");
+
+            }
+        });
+    }
+    public float BMICalculate (float weight, float height) {
+        return weight / (height * height);
+    }
+    public String interpreteBMI (float bmivalue) {
+        if (bmivalue <18.5) {
+            return "Servely Underweight";
+        }
+        else if (bmivalue <25.0){
+            return "Normal";
+        }
+        else if (bmivalue <30) {
+            return "Pre-Obesity";
+        }
+        else if (bmivalue < 35) {
+            return "Obesity class 1";
+        }
+        else if (bmivalue <40) {
+            return "Obesity class 2";
+        }
+        else if (bmivalue < 50) {
+            return "Obesity class 3";
+        }
+        else
+            return "Obese";
+    }
+}
